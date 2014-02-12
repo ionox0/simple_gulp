a0adbedf71b9ef94ac457836cf08fd5204d3161e
ionox0 <ionox0@gmail.com>
2014-02-11 23:25:00 -0800

    added jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,30 @@ module.exports = function(grunt) {
         ignoreLeaks: false,
         reporter: 'tap'
       },
-
+      all: { src: ['test/**/*.js'] }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          require: true,
+          describe: true,
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        },
+      },
       all: { src: ['test/**/*.js'] }
     }
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Add a default task. This is optional, of course :)
   grunt.registerTask('default', 'simplemocha');
